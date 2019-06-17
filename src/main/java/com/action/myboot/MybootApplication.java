33715884a5d34e6a8e193f703fa4212c85e2522a
liuyunfeng2011 <liuyunfeng2011@gmail.com>
2019-06-17 17:02:53 +0800

    mybatis 03

--- src/main/java/com/action/myboot/MybootApplication.java
+++ src/main/java/com/action/myboot/MybootApplication.java
@@ -1,9 +1,11 @@
 package com.action.myboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.action.myboot.mapper")
 public class MybootApplication {
 
 	public static void main(String[] args) {
