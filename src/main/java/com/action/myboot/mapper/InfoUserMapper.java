4fe5d6c8572c07908bc4025b4ee3cbfe2c323317
liuyunfeng2011 <liuyunfeng2011@gmail.com>
2019-06-18 16:28:29 +0800

    redis相关操作

--- src/main/java/com/action/myboot/mapper/InfoUserMapper.java
+++ src/main/java/com/action/myboot/mapper/InfoUserMapper.java
@@ -1,9 +1,10 @@
 package com.action.myboot.mapper;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import com.action.myboot.pojo.InfoUser;
-
+@Repository
 public interface InfoUserMapper {
 	public InfoUser getInfoUser(int infoId);
 	void addInfoUser(@Param("user")InfoUser user);
--- src/test/java/com/action/myboot/service/impl/InfoUserServiceImplTest.java
+++ src/test/java/com/action/myboot/service/impl/InfoUserServiceImplTest.java
@@ -2,21 +2,29 @@ package com.action.myboot.service.impl;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.Resource;
 
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.action.myboot.pojo.InfoUser;
 import com.action.myboot.pojo.SexEnum;
 import com.action.myboot.service.InfoUserService;
+import com.action.myboot.util.RedisUtil;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InfoUserServiceImplTest {
+//	@Resource
+//    private RedisTemplate<String, Object> redisTemplate;
+	@Resource
+    private RedisUtil redisUtil;
 	@Resource(name="infoUserService")
 	private InfoUserService infoUserService;
 	@Ignore
@@ -34,7 +42,14 @@ public class InfoUserServiceImplTest {
 		user.setInfoPwd("admin123");
 		user.setSex(SexEnum.FEMALE);
 		infoUserService.addInfoUser(user);
-		
+	}
+	@Test
+	public void testRedis() {
+//		redisTemplate.opsForValue().set("name", "xiaoheihei");
+	//	redisUtil.set("age", "16");
+	System.out.println(redisUtil.hasKey("age"));	
+	redisUtil.expire("name", 30, TimeUnit.SECONDS);
+	System.out.println(redisUtil.getExpire("name"));
 	}
 	
 
